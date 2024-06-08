c53d4877ffe5bcfa245613753b4bd07ddb579923
Martin Ortiz <maol@mail.maol.dev>
2024-06-08 17:35:24 -0300

    Update useChat.ts

--- src/hooks/useChat.ts
+++ src/hooks/useChat.ts
@@ -12,37 +12,37 @@ const useChat = ({ initialMessages, model }: { initialMessages?: Message[]; mode
   let streamMsg = ''
 
   const fetchData = async (messagesToSend: Message[]) => {
-    try {
-      const res = await fetch('http://localhost:11434/api/chat', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        signal: abortController.current.signal,
-        body: JSON.stringify({
-          model,
-          messages: messagesToSend
-        })
+    const res = await fetch('http://localhost:11434/api/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      signal: abortController.current.signal,
+      body: JSON.stringify({
+        model,
+        messages: messagesToSend
       })
+    })
 
-      if (!res.ok) {
-        throw new Error(res.statusText) // TODO: handle error
-      }
+    if (!res.ok) {
+      throw new Error(res.statusText) // TODO: handle error
+    }
 
-      messagesToSend.push({ role: 'assistant', content: '' })
+    messagesToSend.push({ role: 'assistant', content: '' })
 
-      const decoder = new TextDecoder()
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      for await (const chunk of res.body) {
+    const decoder = new TextDecoder()
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    for await (const chunk of res.body) {
+      try {
         const data = decoder.decode(chunk)
         const json = JSON.parse(data)
         streamMsg += json.message.content
         messagesToSend[messagesToSend.length - 1].content = streamMsg
         setMessages([...messagesToSend])
+      } catch {
+        /* empty */
       }
-    } catch {
-      /* empty */
     }
   }
 
